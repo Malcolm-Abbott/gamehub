999cc5ebdc86fe12e673adb862776389735cb65a
Malcolm Abbott <mjacoding@gmail.com>
2026-03-19 15:02:55 -0700

    adding margin bottom to header for consistency in figma layout

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -4,7 +4,7 @@ import { NavLinks } from './NavLinks';
 export function Header() {
   return (
     <>
-      <header className="sticky top-0 z-20 bg-gradient-to-r from-slate-900 via-slate-800 to-slate-900 shadow-xl border-b border-slate-700/50 py-4">
+      <header className="sticky top-0 z-20 bg-gradient-to-r from-slate-900 via-slate-800 to-slate-900 shadow-xl border-b border-slate-700/50 py-4 mb-8">
         <NavLinks />
       </header>
 
